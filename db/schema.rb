7ccc6c521e04f2fc1bb8d230f4cdc7c669aeb91b
Alain Tran <alaint77@gmail.com>
2024-07-28 23:46:26 +0200

    add articles_controller + related CRUD stuff WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_162507) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_28_164337) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
